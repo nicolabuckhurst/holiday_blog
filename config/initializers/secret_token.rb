a50c6757097ae90399537a7b78f1688b6c5d10b0
Nicola Buckhurst <nicola@buckhurst.org>
2013-04-15 22:38:59 +0100

    initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayBlog::Application.config.secret_token = 'b68b56d340138c805503fc2d1d201b178859794e444acd7b9c9b674f98b6c91815369abb32573d3649893ca1a81918dd3ce30ddc83f411b74a4fdac8e69cc91a'
